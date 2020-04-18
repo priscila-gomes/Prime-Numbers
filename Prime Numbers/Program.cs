5b7bdcd45b3003fb932fd660c4370086f5455b5b
priscila-gomes <koniloni@yahoo.com>
2020-04-18 17:53:36 -0400

    Add project files.

---
+++ Prime Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
